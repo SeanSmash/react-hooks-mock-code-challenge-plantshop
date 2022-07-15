6d3dcb39d6f2fa8add7a51f52c15593a9b3e74af
Sean Sarokas <sps4462@gmail.com>
2022-07-15 07:40:30 -0400

    commit tree 45904809b23fef973e4717127cd10a28c1d9a126

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(r => r.json())
+    .then(data => setPlants(data))
+  }, [])
+
+  function handleNewPlant(newPlant){
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify( {
+        "name": newPlant.name,
+        "image": newPlant.image,
+        "price": newPlant.price
+      } )
+    })
+    .then(r => r.json())
+    .then(data => setPlants([...plants, data]))
+  }
+
+  function handleSearch(searchTerm){
+    setSearch(searchTerm)
+  }
+  const plantsToDisplay = plants.filter(plant =>{
+    if (search.length > 0) {
+      return (plant.name.includes(search))
+    } else {
+      return true
+    }
+  })
+
+  function handleDelete(id){
+    const updatedPlants = plants.filter(plant => plant.id === id ? null : true)
+    setPlants(updatedPlants)
+  }
+
+  function handlePriceChange(priceChangedPlant){
+    const updatedPrices = plants.map(plant => plant.id === priceChangedPlant.id ? priceChangedPlant : plant)
+    setPlants(updatedPrices)
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onNewPlant={handleNewPlant}/>
+      <Search onSearch={handleSearch}/>
+      <PlantList 
+        plants={plantsToDisplay} 
+        onDelete={handleDelete}
+        onPriceChange={handlePriceChange} />
     </main>
   );
 }
